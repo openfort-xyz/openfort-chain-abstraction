47fc05a70d089dfe85e4e70e155316231c69dfec
banasa44 <cfbennassar@gmail.com>
2024-12-19 13:42:57 +0100

    tests and script for sepolia WiP

--- src/vaults/AaveVault.sol
+++ src/vaults/AaveVault.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "forge-std/console.sol";
+
 import {IVault} from "../interfaces/IVault.sol";
 import {BaseVault} from "./BaseVault.sol";
 import {IVaultManager} from "../interfaces/IVaultManager.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-import {IL2Pool} from "aave-v3-origin/core/contracts/interfaces/IL2Pool.sol";
-import {L2Encoder} from "aave-v3-origin/core/contracts/misc/L2Encoder.sol";
+import {IPool} from "aave-v3-origin/core/contracts/interfaces/IPool.sol";
+// import {L2Encoder} from "aave-v3-origin/core/contracts/misc/L2Encoder.sol";
 import {AaveProtocolDataProvider} from "aave-v3-origin/core/contracts/misc/AaveProtocolDataProvider.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -18,8 +20,8 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 contract AaveVault is BaseVault {
     using SafeERC20 for IERC20;
     // Aave necessary libraries
-    IL2Pool public aavePool;
-    L2Encoder public l2Encoder;
+    IPool public aavePool;
+    // L2Encoder public l2Encoder;
     AaveProtocolDataProvider public dataProvider;
     /// @notice The total amount of shares.
     uint256 public totalYieldShares;
@@ -27,9 +29,12 @@ contract AaveVault is BaseVault {
     function initialize(
         IVaultManager _vaultManager,
         IERC20 _underlyingToken, // The token managed by this vault -- the aToken minted in a 1:1 ratio by Aave protocol
-        IL2Pool _aavePool,
-        L2Encoder _l2Encoder // , AaveProtocolDataProvider _dataProvider
-    ) public initializer {
+        IPool _aavePool
+    )
+        public
+        // L2Encoder _l2Encoder // , AaveProtocolDataProvider _dataProvider
+        initializer
+    {
         require(
             address(_aavePool) != address(0),
             "AaveVault: invalid Aave Pool address"
@@ -40,7 +45,7 @@ contract AaveVault is BaseVault {
         );
 
         aavePool = _aavePool;
-        l2Encoder = _l2Encoder;
+        // l2Encoder = _l2Encoder;
         // dataProvider = _dataProvider;
         // Initialize BaseVault
         super.initialize(_vaultManager, _underlyingToken);
@@ -51,17 +56,35 @@ contract AaveVault is BaseVault {
         uint256 amount,
         bool isYield
     ) internal override {
+        // Log the initial balance of the underlying token in the vault
+        uint256 initialVaultBalance = token.balanceOf(address(this));
+        console.log(
+            "Vault Initial Underlying Token Balance:",
+            initialVaultBalance
+        );
+
+        // Approve the Aave Pool to spend the underlying tokens
         underlyingToken.approve(address(aavePool), amount);
 
-        bytes32 args = l2Encoder.encodeSupplyParams(
-            address(underlyingToken),
-            amount,
-            0
+        // Log the allowance from the vault to the Aave Pool after approval
+        uint256 allowanceToPool = underlyingToken.allowance(
+            address(this),
+            address(aavePool)
+        );
+        console.log(
+            "Allowance from Vault to Aave Pool after approve:",
+            allowanceToPool
         );
 
         // Perform a low-level call to the `supply` function of the Aave Pool
         (bool success, ) = address(aavePool).call(
-            abi.encodeWithSelector(IL2Pool.supply.selector, args)
+            abi.encodeWithSelector(
+                IPool.supply.selector,
+                address(underlyingToken), // Underlying token
+                amount, // Amount to supply
+                address(this), // On-behalf-of address
+                0 // Referral code
+            )
         );
         require(success, "AaveVault: Supply to Aave failed");
     }
@@ -71,18 +94,23 @@ contract AaveVault is BaseVault {
         uint256 amount,
         address recipient
     ) internal override {
-        bytes32 args = l2Encoder.encodeWithdrawParams(address(token), amount);
+        // bytes32 args = l2Encoder.encodeWithdrawParams(address(token), amount);
 
         // Perform a low-level call to the `withdraw` function of the Aave Pool
-        (bool success, bytes memory returnData) = address(aavePool).call(
-            abi.encodeWithSelector(IL2Pool.withdraw.selector, args)
+        (bool success, ) = address(aavePool).call(
+            abi.encodeWithSelector(
+                IPool.withdraw.selector,
+                token,
+                amount,
+                recipient
+            )
         );
 
         // Revert the transaction if the call fails
         require(success, "AaveVault: Withdraw from Aave failed");
 
-        uint256 withdrawnAmount = abi.decode(returnData, (uint256));
+        // uint256 withdrawnAmount = abi.decode(returnData, (uint256));
 
-        token.safeTransfer(recipient, withdrawnAmount);
+        // token.safeTransfer(recipient, withdrawnAmount);
     }
 }
